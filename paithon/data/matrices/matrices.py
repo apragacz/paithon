fe509c38d274118354b85f85653d53ab2cf3b7ad
Andrzej Pragacz <andrzej.pragacz@10clouds.com>
2012-08-08 09:37:53 +0200

    matrices

--- paithon/data/matrices/matrices.py
+++ paithon/data/matrices/matrices.py
@@ -1,6 +1,30 @@
 
 
+class SparseRow(object):
+    def __init__(self, default_value=None):
+        self._data = {}
+        self._default = default_value
+
+    def _getitem__(self, key):
+        self._data.get(key, self._default)
+
+    def _setitem_(self, key, value):
+        if value == self._default:
+            if key in self._data:
+                del self._data[key]
+        else:
+            self._data[key] = value
+
+
 class SparseMatrix(object):
     def __init__(self, default_value=None):
         self._data = {}
-        self._default = default
+        self._default = default_value
+
+    def _getitem__(self, key):
+        if isinstance(key, tuple) and len(key) == 2:
+            x, y = key
+            return self[x][y]
+        else:
+            self._data.setdefault(key, SparseRow(default_value=self._default))
+            return self._data[key]
